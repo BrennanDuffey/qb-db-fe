e16edc28673701c31763ffa8fd610c986c41f1a2
Brennan <brennanduffey@gmail.com>
2019-06-04 14:54:54 -0600

    Finish functionality for quiz

--- src/containers/QuizContainer/index.js
+++ src/containers/QuizContainer/index.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from "react-router-dom";
+
 
 class QuizContainer extends Component {
-  constructor({ tossups }) {
+  constructor() {
+    super();
     this.state = {
-      questionCounter: 0
+      questionCounter: 0,
       displayAnswer: false
     }
   }
 
+  incrementCounter = () => {
+    const questionCounter = this.state.questionCounter++
+    this.setState({ questionCounter })
+  }
+
+  toggleDisplay = () => {
+    this.setState({ displayAnswer: !this.state.displayAnswer })
+  }
+
 
   render() {
+    if (!this.props.tossups.length) {
+      return <Redirect to={'/Controls'}/>
+    }
+    const { tossups } = this.props
+    const { displayAnswer, questionCounter } = this.state
     return (
       <section>
+        {!displayAnswer && <p>{tossups[questionCounter].text}</p>}
+        {!displayAnswer && <h3 onClick={this.toggleDisplay}>Show Answer </h3>}
+        {displayAnswer && <p>{tossups[questionCounter].answer}</p>}
+        {
+          displayAnswer && <h3 onClick={() => {
+          this.toggleDisplay()
+          this.incrementCounter()
+          }}>Next Question</h3>
+        }
 
       </section>
     )
   }
 }
 
-export default connect(null)(QuizContainer)
+export const mapStateToProps = (state) => ({
+  tossups: state.tossups
+});
+
+export default connect(mapStateToProps)(QuizContainer)
