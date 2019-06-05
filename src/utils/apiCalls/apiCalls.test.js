b9602c9d5571858b13b6f6214dd7d651eca26e9f
Brennan <brennanduffey@gmail.com>
2019-06-05 06:43:43 -0600

    Add all tests for fetchWithCount apiCall

--- src/utils/apiCalls/apiCalls.test.js
+++ src/utils/apiCalls/apiCalls.test.js
@@ -2,25 +2,44 @@ import { fetchWithCount, fetchWithOptions } from './apiCalls';
 import { mockTossups } from '../mockData';
 
 describe('apiCalls', () => {
+  let mockCount;
+  let mockSelectedCategories;
+
   beforeEach(() => {
+    mockCount = 15;
+    mockSelectedCategories = [19, 25]
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
-        json: () => Promise.resolve(mockTossups)
-      })
-    })
-  })
-
-  describe('fetchWithCount', () => {
-    it('should be called with the correct pararms', () => {
-      const mockCount = 15;
-      const expected = 'http://localhost:3000/api/v1/tossups/15';
-      fetchWithCount(mockCount);
-      expect(window.fetch).toHaveBeenCalledWith(expected)
+        json: () => Promise.resolve({ tossups: mockTossups})
+      });
     });
   });
 
   describe('fetchWithCount', () => {
+    it('should be called with the correct pararms', () => {
+      const expected = 'http://localhost:3000/api/v1/tossups/15';
+      fetchWithCount(mockCount);
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    });
+
+    it('should return correct response if ok', async () => {
+      const result = await fetchWithCount(mockCount);
+      expect(result.tossups).toEqual(mockTossups);
+    });
+
+    it('should return an error if status is not ok', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      })
+
+      await expect(fetchWithCount(mockCount)).rejects.toEqual(Error('Hmm something went wrong please refresh the page'));
+    });
+  });
+
+  describe('fetchWithOptions', () => {
 
   });
 });
