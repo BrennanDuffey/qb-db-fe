eb42a181c58f900790d143a70f85cdf5b926e38f
Brennan <brennanduffey@gmail.com>
2019-06-04 18:42:37 -0600

    Actions tested

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,7 +1,27 @@
 import * as actions from '../actions';
+import { mockTossups } from '../utils/mockData';
 
 describe('actions', () => {
-  it('should have a type of ADD_TODO', () => {
-    expect(true).toEqual(false)
+  describe('setTossups', () => {
+    it('should have a type of SET_TOSSUPS', () => {
+      const expected = 'SET_TOSSUPS';
+      const result = actions.setTossups(mockTossups);
+      expect(result.type).toEqual(expected);
+    }); 
+
+    it('should have a payload of tossups', () => {
+      const expected = mockTossups;
+      const result = actions.setTossups(mockTossups);
+      expect(result.tossups).toEqual(expected);
+    });
+  });
+
+  describe('toggleLoading', () => {
+    it('should create an object with a type of TOGGLE_LOADING', () => {
+      const expected = { type: 'TOGGLE_LOADING' };
+      const result = actions.toggleLoading();
+      expect(result).toEqual(expected);
+    });
+
   });
 });
---
+++ src/utils/mockData/index.js
@@ -0,0 +1,90 @@
+export const mockTossups = [
+  {
+    "id": 123902,
+    "text": "Description acceptable. A paper by Urribarri, Rios, and Ferrer hoped that treatment with ammonia would allow this process to be used as a source of raw vegetable protein for the feeding of livestock. Pollution with poliaspartic acid and disruption of the hypolemnitic cone have been suggested as causes of this phenomenon, which may result in lower (*) oxygen levels that threaten the curbina fishery. Researchers at the University of Zulia blamed ships from Florida or Texas for starting this phenomenon by transporting an  ► invasive species of genus Lemna. For 10 points, what bloom of aquatic plants threatens the ecological health of a certain lake in western Venezuela?",
+    "answer": "duckweed infestation of Lake Maracaibo (accept equivalents)",
+    "number": 3,
+    "tournament_id": null,
+    "category_id": 20,
+    "subcategory_id": null,
+    "round": "[missing]",
+    "created_at": "2017-08-27T22:44:27.515Z",
+    "updated_at": "2017-09-01T07:10:28.156Z",
+    "quinterest_id": 64463,
+    "formatted_text": "<b>Description acceptable. A paper by Urribarri, Rios, and Ferrer hoped that treatment with ammonia would allow this process to be used as a source of raw vegetable protein for the feeding of livestock. Pollution with poliaspartic acid and disruption of the hypolemnitic cone have been suggested as causes of this phenomenon, which may result in lower (*)</b>) oxygen levels that threaten the curbina fishery. Researchers at the University of Zulia blamed ships from Florida or Texas for starting this phenomenon by transporting an  &#9658; invasive species of genus Lemna. For 10 points, what bloom of aquatic plants threatens the ecological health of a certain lake in western Venezuela?",
+    "formatted_answer": "duckweed infestation of Lake Maracaibo (accept equivalents)",
+    "wikipedia_url": null,
+    "url": "https://www.quizdb.org/api/tossups/123902",
+    "type": "tossup",
+    "tournament": {
+    },
+    "category": {
+      "id": 20,
+      "name": "Geography",
+      "created_at": "2017-08-27T19:30:45.889Z",
+      "updated_at": "2017-08-27T19:30:45.889Z",
+      "url": "https://www.quizdb.org/api/categories/20"
+    },
+    "subcategory": {
+    }
+  },
+  {
+    "id": 123907,
+    "text": "The \"Spreuer\" example of this feature in Switzerland contains Kaspar Meglinger's 17th-century paintings of the Danse Macabre. Examples of these features from Qing Dynasty China include the \"stepping toad\" and the \"eternal celebration.\" The longest feature of this type in the world, at Hartland in New Brunswick, was damaged by ice in the 1920s. The \"Burr (*) arch\" is a typical design for these structures, which exist at Kaufman's Distillery, White Rock Forge, and Landis Mill in Lancaster County, Pennsylvania. A 1992 novel by ► Robert James Waller centers on a photographer who is assigned to document these features in Madison County, Iowa. For 10 points, identify this kind of river crossing that is protected from rain and snow.",
+    "answer": "covered bridges (prompt on \"bridges\")",
+    "number": 8,
+    "tournament_id": null,
+    "category_id": 20,
+    "subcategory_id": null,
+    "round": "[missing]",
+    "created_at": "2017-08-27T22:44:27.524Z",
+    "updated_at": "2017-09-01T17:24:27.978Z",
+    "quinterest_id": 64468,
+    "formatted_text": "<b>The \"Spreuer\" example of this feature in Switzerland contains Kaspar Meglinger's 17th-century paintings of the Danse Macabre. Examples of these features from Qing Dynasty China include the \"stepping toad\" and the \"eternal celebration.\" The longest feature of this type in the world, at Hartland in New Brunswick, was damaged by ice in the 1920s. The \"Burr (*)</b>) arch\" is a typical design for these structures, which exist at Kaufman's Distillery, White Rock Forge, and Landis Mill in Lancaster County, Pennsylvania. A 1992 novel by &#9658; Robert James Waller centers on a photographer who is assigned to document these features in Madison County, Iowa. For 10 points, identify this kind of river crossing that is protected from rain and snow.",
+    "formatted_answer": "covered bridges (prompt on \"bridges\")",
+    "wikipedia_url": null,
+    "url": "https://www.quizdb.org/api/tossups/123907",
+    "type": "tossup",
+    "tournament": {
+    },
+    "category": {
+      "id": 20,
+      "name": "Geography",
+      "created_at": "2017-08-27T19:30:45.889Z",
+      "updated_at": "2017-08-27T19:30:45.889Z",
+      "url": "https://www.quizdb.org/api/categories/20"
+    },
+    "subcategory": {
+    }
+  }
+];
+
+export const mockTossup = {
+"id": 123907,
+  "text": "The \"Spreuer\" example of this feature in Switzerland contains Kaspar Meglinger's 17th-century paintings of the Danse Macabre. Examples of these features from Qing Dynasty China include the \"stepping toad\" and the \"eternal celebration.\" The longest feature of this type in the world, at Hartland in New Brunswick, was damaged by ice in the 1920s. The \"Burr (*) arch\" is a typical design for these structures, which exist at Kaufman's Distillery, White Rock Forge, and Landis Mill in Lancaster County, Pennsylvania. A 1992 novel by ► Robert James Waller centers on a photographer who is assigned to document these features in Madison County, Iowa. For 10 points, identify this kind of river crossing that is protected from rain and snow.",
+  "answer": "covered bridges (prompt on \"bridges\")",
+  "number": 8,
+  "tournament_id": null,
+  "category_id": 20,
+  "subcategory_id": null,
+  "round": "[missing]",
+  "created_at": "2017-08-27T22:44:27.524Z",
+  "updated_at": "2017-09-01T17:24:27.978Z",
+  "quinterest_id": 64468,
+  "formatted_text": "<b>The \"Spreuer\" example of this feature in Switzerland contains Kaspar Meglinger's 17th-century paintings of the Danse Macabre. Examples of these features from Qing Dynasty China include the \"stepping toad\" and the \"eternal celebration.\" The longest feature of this type in the world, at Hartland in New Brunswick, was damaged by ice in the 1920s. The \"Burr (*)</b>) arch\" is a typical design for these structures, which exist at Kaufman's Distillery, White Rock Forge, and Landis Mill in Lancaster County, Pennsylvania. A 1992 novel by &#9658; Robert James Waller centers on a photographer who is assigned to document these features in Madison County, Iowa. For 10 points, identify this kind of river crossing that is protected from rain and snow.",
+  "formatted_answer": "covered bridges (prompt on \"bridges\")",
+  "wikipedia_url": null,
+  "url": "https://www.quizdb.org/api/tossups/123907",
+  "type": "tossup",
+  "tournament": {
+  },
+  "category": {
+    "id": 20,
+    "name": "Geography",
+    "created_at": "2017-08-27T19:30:45.889Z",
+    "updated_at": "2017-08-27T19:30:45.889Z",
+    "url": "https://www.quizdb.org/api/categories/20"
+  },
+  "subcategory": {
+  }
+}
