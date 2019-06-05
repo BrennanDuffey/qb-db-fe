45504f848b724e13eaec6b15d91f6ea6588f84eb
Brennan <brennanduffey@gmail.com>
2019-06-04 19:12:18 -0600

    Add mount and shallow render tests for App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,20 +8,9 @@ import StudyContainer from '../../containers/StudyContainer';
 
 
 function App() {
-
-const grabTossups = async () => {
-  const response = await fetch('http://localhost:3000/api/v1/tossups/22&18/100');
-  console.log('response', response)
-  if (!response.ok) {
-    throw Error('Error fetching tossups')
-  }
-  const tossups = await response.json()
-  console.log(tossups)
-}
-
   return (
     <main className="">
-      <button onClick={grabTossups}>Stuff </button>
+      <button>Stuff </button>
       <Route exact path='/' component={SplashPage} />
       <Route exact path='/Controls' component={Controls} />
       <Route exact path='/Quiz' component={QuizContainer} />
