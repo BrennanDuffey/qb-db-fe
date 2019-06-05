f06e3752cd93170826f9d1f4fe99cd75c2ead14c
Brennan <brennanduffey@gmail.com>
2019-06-04 19:30:54 -0600

    Add test for default state of SplashPage

--- src/components/SplashPage/SplashPage.test.js
+++ src/components/SplashPage/SplashPage.test.js
@@ -7,12 +7,14 @@ describe('Card', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<SplashPage />)
+    wrapper = shallow(<SplashPage />);
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  
+  it('should have correct default state', () => {
+    expect(wrapper.state('continue')).toEqual(false);
+  });
 });
