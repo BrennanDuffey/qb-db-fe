4ef5757db8c971f6cc7e319b8419c43c88b844b9
Brennan <brennanduffey@gmail.com>
2019-06-05 06:47:44 -0600

    Add tests for fetchWithOptions apiCall

--- src/utils/apiCalls/apiCalls.test.js
+++ src/utils/apiCalls/apiCalls.test.js
@@ -32,14 +32,31 @@ describe('apiCalls', () => {
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
           ok: false
-        })
-      })
-
+        });
+      });
       await expect(fetchWithCount(mockCount)).rejects.toEqual(Error('Hmm something went wrong please refresh the page'));
     });
   });
 
   describe('fetchWithOptions', () => {
+    it('should be called with the correct pararms', () => {
+      const expected = 'http://localhost:3000/api/v1/tossups/19&25/15';
+      fetchWithOptions(mockCount, mockSelectedCategories);
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    });
 
+    it('should return correct response if ok', async () => {
+      const result = await fetchWithOptions(mockCount, mockSelectedCategories);
+      expect(result.tossups).toEqual(mockTossups);
+    });
+
+    it('should return an error if status is not ok', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      await expect(fetchWithCount(mockCount)).rejects.toEqual(Error('Hmm something went wrong please refresh the page'));
+    });
   });
 });
